4f0f729147214567b347c16fc114b2696b917287
Aaron Santana <84732450+Santaval@users.noreply.github.com>
2025-03-25 19:06:07 -0600

    Add Holder class for managing buffers and semaphores in thread operations

---
+++ src/Holder.hpp
@@ -0,0 +1,17 @@
+
+#pragma once
+#include "Thread.hpp"
+
+class Holder: public Thread {
+  private:
+    Buffer* client_holder_buffer;
+    Buffer* holder_server_buffer;
+    sem_t* client_request_sem;
+    sem_t* mktp_request_sem;
+    sem_t* mktp_response_sem;
+  public: 
+    Holder(Buffer* client_holder_buffer, Buffer* holder_server_buffer, sem_t* client_request_sem, sem_t* mktp_request_sem, sem_t* mktp_response_sem) :
+      client_holder_buffer(client_holder_buffer), holder_server_buffer(holder_server_buffer), client_request_sem(client_request_sem), mktp_request_sem(mktp_request_sem), mktp_response_sem(mktp_response_sem) {}
+    int run() override;
+    ~Holder() = default;
+};
