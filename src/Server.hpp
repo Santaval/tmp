d9e6fcd261f73afd3216544852b19febe895934f
Aaron Santana <84732450+Santaval@users.noreply.github.com>
2025-03-25 19:11:21 -0600

    Add Server class for handling server operations with threading support

---
+++ src/Server.hpp
@@ -0,0 +1,15 @@
+
+#pragma once
+#include "Thread.hpp"
+
+class Server: public Thread {
+  private:
+    Buffer* holder_server_buffer;
+    sem_t* mktp_request_sem;
+    sem_t* mktp_response_sem;
+  public: 
+    Server(Buffer* holder_server_buffer, sem_t* mktp_request_sem, sem_t* mktp_response_sem) :
+      holder_server_buffer(holder_server_buffer), mktp_request_sem(mktp_request_sem), mktp_response_sem(mktp_response_sem) {};
+    ~Server() = default;
+    int run() override;
+};
