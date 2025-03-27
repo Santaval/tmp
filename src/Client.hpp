46d5e5698594aeabed1c7cf726c96cc0b6f7dfcd
Jesus Porras <101618898+Jairus-ai@users.noreply.github.com>
2025-03-26 20:33:22 -0600

    doc(Simulation): documentar client.hpp

--- src/Client.hpp
+++ src/Client.hpp
@@ -6,18 +6,30 @@
 
 class Client: public Thread {
   private:
+    /// @brief  Shared buffer with the holder to send messages.
     Buffer* buffer;
+    /// @brief Semaphore to send request
     sem_t* client_request_sem;
+    /// @brief Semaphore to wait for a response.
     sem_t* client_response_sem;
-  public: 
+  public:
+    /// @brief Constructor
     Client(Buffer* buffer, sem_t* client_request_sem, sem_t* client_response_sem) :
       buffer(buffer),
       client_request_sem(client_request_sem),
       client_response_sem(client_response_sem)
       {}
+    /// @brief Destructor
     ~Client() = default;
+    /// @brief Execute thread
+    /// @return Returns state value. O if success
     int run() override;
+    /// @brief Sends the HTTP request to the holder
+    /// @param request The request to be make to the serer. Written in HTTP
+    /// @return Returns state value. O if success
     int sendHTTP(const char*);
+    /// @brief Receives and prints the response from the server.
+    /// @return Returns state value. O if success
     int manageResponse();
-    
+
 };
