3af8571fbc9a7168b362cd00f3993f8326c086d7
Aaron Meza <aaron.meza@ucr.ac.cr>
2025-03-26 19:38:23 -0600

    Implementando la clase Server

--- src/Server.hpp
+++ src/Server.hpp
@@ -9,9 +9,15 @@ class Server: public Thread {
     Buffer* holder_server_buffer;
     sem_t* mktp_request_sem;
     sem_t* mktp_response_sem;
+    std::string image;
+    std::string image_2;
+    std::string image_3;
+    std::string image_4;
+    std::string image_5;
   public: 
     Server(Buffer* holder_server_buffer, sem_t* mktp_request_sem, sem_t* mktp_response_sem) :
       holder_server_buffer(holder_server_buffer), mktp_request_sem(mktp_request_sem), mktp_response_sem(mktp_response_sem) {};
     ~Server() = default;
     int run() override;
+    int answerMKTP();
 };
